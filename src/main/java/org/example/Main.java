8707fa6f58ad90d5d891e8c7a2ebd67e53548331
Bhargavi <venkatabhargavigopisetty@gmail.com>
2023-11-01 14:22:02 +0530

    Main.java is committed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,15 @@
 package org.example;
-
-public class Main {
-    public static int add(int number1, int number2) {
-        return number1 + number2;
+interface car {
+    public void drive();
+}
+class benz implements car{
+    public void drive() {
+        System.out.println("Driving");
     }
-
-    public static int sub(int number1, int number2) {
-        return number1 - number2;
-    }
-
-    public static int mul(int number1, int number2) {
-        return number1 * number2;
-    }
-
-    //    public int div(int number1,int number2) {
-//        return number1 / number2;
-//    }
-
-
-//    public double div(double number1,double number2) {
-//        if (number2 == 0) {
-//            throw new IllegalArgumentException("Number cannot be divided by 0");
-//        }
-//        return number1 / number2;
-    public static int div(int number1, int number2) {
-        if (number2 == 0) {
-            throw new ArithmeticException("Not Divided by 0");
-        }
-        return number1/number2;
+}
+class Main{
+    public static void main(String[] args) {
+        benz obj=new benz();
+        obj.drive();
     }
 }
